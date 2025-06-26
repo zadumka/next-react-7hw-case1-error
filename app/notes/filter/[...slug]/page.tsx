24e5a499387ac18ab1962684f81116502eeadcf8
zadumka <90329284+zadumka@users.noreply.github.com>
2025-06-26 14:40:56 +0300

    Update page.tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,16 +1,21 @@
-import { fetchNotes } from '@/lib/api';
-import NotesClient from './Notes.client';
-import { Tag } from '@/types/note.js';
+import { Tags } from "@/types/note";
+import NotesClient from "./Notes.client";
+import { fetchNotes } from "@/lib/api";
 
-export default async function NotesPage({ params }: { params: { slug: string[] } }) {
-  const { slug } = await params;
-  const tag: Tag | string = slug[0];
+export default async function NotesPage({
+  params,
+}: {
+  params: Promise<{ slug: [Tags | "All", ...string[]] }>;
+}) {
+  const { slug: tag } = await params;
 
-  const data = await fetchNotes({
-    searchText: '',
-    page: 1,
-    ...(tag && tag !== 'All' && { tag }),
-  });
+  const tagFilter: Tags | "" = tag[0] === "All" ? "" : tag[0];
 
-  return <NotesClient initialData={data} tag={tag} />;
+  const data = await fetchNotes({ tag: tagFilter });
+
+  return (
+    <div>
+      <NotesClient initialData={data} tag={tagFilter} />
+    </div>
+  );
 }
