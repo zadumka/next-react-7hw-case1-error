27598044e0d299432e8cefdff8d24bc89598296e
zadumka <90329284+zadumka@users.noreply.github.com>
2025-10-17 13:52:04 +0300

    Refactor Header component to use TagsMenu

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,23 +1,15 @@
 import Link from 'next/link';
 import css from './Header.module.css';
+import TagsMenu from '../TagsMenu/TagsMenu';
 
 export default function Header() {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home" className={css.headerLink}>
-        NoteHub
-      </Link>
+      <div className={css.headerLink}>NoteHub</div>
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
           <li className={css.navigationItem}>
-            <Link className={css.navigationLink} href="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className={css.navigationLink} href="/notes/filter/all">
-              Notes
-            </Link>
+            <TagsMenu />
           </li>
         </ul>
       </nav>
